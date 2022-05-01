cb75f8b997ae9d6502738190743ee5c441d530a7
NVRM0R <vovavelho@gmail.com>
2022-05-01 06:37:21 -0400

    MVP is ready, requeires cluster folder

--- server.py
+++ server.py
@@ -7,11 +7,15 @@ from logging import exception
 from sre_constants import SUCCESS
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin
+from scipy.io import loadmat
+from threading import Thread
 
 from os import listdir,mkdir,path,rmdir,remove
 import glob
 from os.path import exists
 import sys
+sys.path.append(path.join(sys.path[0], '../cluster_roi'))
+import clustering as clustering
 
 app = Flask('__name__')
 cors = CORS(app)
@@ -28,24 +32,40 @@ def loadProject(projectName):
     anatomicalB64 = base64.b64encode(anatomical).decode('utf-8')
     return anatomicalB64
 
-def newProject(projectName,file):
+def newProject(projectName,file,connProject):
     folderPath = path.join("filePoint",projectName.split('.')[0])
     if not path.exists(folderPath):
         mkdir(folderPath,mode=0o777)
-    if "gz" in projectName:
-        f = open(path.join(folderPath,"anatomical.nii.gz"),"wb")
+    connFile = open(path.join(folderPath,projectName),"wb")
+    connFile.write(file)
+    connFile.close()
+    if(connProject=='true'):
+        print("Loaded CONN project",file=sys.stderr)
+        matData = loadmat(path.join(folderPath,projectName))['CONN_x']
+        func = matData['Setup'][0][0][0][0]['functional'][0][0][0][0][0][0][0]
+        anat = matData['Setup'][0][0][0][0]['rois']['files'][0][0][0][0][0][0][0][0][0][0][0]
     else:
-        f = open(path.join(folderPath,"anatomical.nii"),"wb")
-    f.write(file)
-    f.close()
+        func = ''
+        anat = ''
     data = {
         "name":projectName,
-        "status":"new", # new,ready, parcellating 
+        "status":"new", # new,calculating,ready, parcellating
         "parc":['Исходный снимок'],
         "editable":"no",
     }
     with open(path.join(folderPath,'info.json'), 'w') as fp:
         json.dump(data, fp)
+    print({'func':func,'anat':anat},file=sys.stderr)
+    return {'func':func,'anat':anat}
+
+def saveFile(projectName,anatFile,funcFile):
+    folderPath = path.join("filePoint",projectName.split('.')[0])
+    aFile = open(path.join(folderPath,"anatomical.nii"),"wb")
+    aFile.write(anatFile)
+    aFile.close()
+    fFile = open(path.join(folderPath,"functional.nii"),"wb")
+    fFile.write(funcFile)
+    fFile.close()
 
 
 @app.route("/listProjects",methods=['GET'])
@@ -59,21 +79,74 @@ def listProjectHandler():
 @app.route("/newProject",methods=['POST'])
 @cross_origin()
 def newProjectHandler():
-    file = request.files.get('anatomical').read()
+    file = request.files.get('connFile').read()
     projectName = request.headers.get('projectName')
-    newProject(projectName,file)
-    resp = jsonify(success=True)
-    return resp
+    connProject = request.headers.get('connProject')
+    paths = newProject(projectName,file,connProject)
+    paths['success'] = True
+    return jsonify(paths)
+
+
+def workloadCalculateMatrix(projectPath):
+    clustering.calculateMatrix("../vega/"+projectPath)
+    
+    jsonFile = path.join(projectPath,'info.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+        data['status'] = 'ready'
+    with open(jsonFile, 'w') as file:
+        json.dump(data, file)
+    return
+
+def workloadParcellate(projectPath,k):
+    clustering.cluster("../vega/"+projectPath,k)
+    
+    jsonFile = path.join(projectPath,'info.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+        data['status'] = 'ready'
+        listOfParcs = data['parc']
+        listOfParcs.append(str(k))
+        data['parc'] = listOfParcs
+    with open(jsonFile, 'w') as file:
+        json.dump(data, file)
+    return
+
+
+@app.route("/uploadProjectFiles",methods=['POST'])
+@cross_origin()
+def uploadProjectHandler():
+    anatFile = request.files.get('anatomical').read()
+    funcFile = request.files.get('functional').read()
+    projectName = request.headers.get('projectName')
+    saveFile(projectName,anatFile,funcFile)
+    folderPath = path.join("filePoint",projectName.split('.')[0])
+    jsonFile = path.join(folderPath,'info.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+        data['status'] = 'calculating'
+    with open(jsonFile, 'w') as file:
+        json.dump(data, file)
+    thread = Thread(target=workloadCalculateMatrix,args=(folderPath,))
+    thread.start()
+    return jsonify({'success':True}) 
 
 @app.route('/data/<path:path>')
 @cross_origin()
 def send_volume(path):
-    fStr = 'filePoint/'+path+'/anatomical.nii'
-    fStrGz = 'filePoint/'+path+'/anatomical.nii.gz'
+    vals = path.split('@')
+    k = 0
+    if(len(vals)>1):
+        projectName,k = vals
+    else:
+        projectName = vals[0]
+    if(k == 0):
+        fStr = 'filePoint/'+projectName+'/anatomical.nii'
+    else:
+        fStr = 'filePoint/'+projectName+'/cluster_'+str(k)+'_BIG.nii'
+    print(fStr, file=sys.stderr)
     if exists(fStr):
         return send_from_directory('.', fStr)
-    elif exists(fStrGz):
-        return send_from_directory('.', fStrGz)
     else:
         return "not found"
 
@@ -85,6 +158,23 @@ def send_info(path):
     with open(fStr) as fp:
         data = json.load(fp)
     return data
+
+@app.route('/parcellate')
+@cross_origin()
+def parcellate():
+    projectName = request.headers.get('projectName')
+    k = request.headers.get('clusters')
+    jsonFile = path.join("filePoint",projectName,'info.json')
+    with open(jsonFile, 'r') as file:
+        data = json.load(file)
+        data['status'] = 'parcellating'
+    with open(jsonFile, 'w') as file:
+        json.dump(data, file)
+
+    folderPath = path.join("filePoint",projectName)
+    thread = Thread(target=workloadParcellate,args=(folderPath,int(k),))
+    thread.start()
+    return jsonify({'success':True})  
 
 @app.route('/delete/<path>')
 @cross_origin()
