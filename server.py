f6f8f8021b3e52325eaa8cbea16b43684e61f5db
NVRM0R <vovavelho@gmail.com>
2022-06-05 22:56:07 +0300

    comments added

--- server.py
+++ server.py
@@ -1,4 +1,20 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+
+####
+# 2022 Vladimir Andreev (andreev.va2010@yandex.ru)
+# as a part of bachelor theses
+# Server math side of web-based brain parcellation server 
+#
+# Functionality:
+#   + handles user requests
+#   + stores projects and files
+#   + run methods, algorithms for brain parcellation 
+#   
+# This file is a flask server for web application.
+# NOTE: print send data into stderr pipe for FLASK logging
+#
+####
+
 from crypt import methods
 import json
 from logging import exception
@@ -19,11 +35,14 @@ app = Flask('__name__')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# internal method for getting available projects
+# return: list of projects in working directory
 def getProjects():
     result = listdir("filePoint/")
     return result
 
-
+# method for handling listProject request
+# return: sends to client list of available project in JSON style
 @app.route("/listProjects",methods=['GET'])
 @cross_origin()
 def listProjectHandler():
@@ -31,6 +50,17 @@ def listProjectHandler():
     resp = jsonify(list=localPrjList)
     return resp
 
+# method for handling newProject request
+# header:
+#   projectName: name of new project
+#   connProject: flag CONN/non-CONN file
+# connFile: root file of CONN project
+# return: JSON object with success=true if OK.
+#         if the project is CONN based,
+#         return JSON object will appended
+#         with anatanomical (graymatter) and 
+#         functional paths, to help user
+#         load files.
 @app.route("/newProject",methods=['POST'])
 @cross_origin()
 def newProjectHandler():
@@ -41,7 +71,18 @@ def newProjectHandler():
     paths['success'] = True
     return jsonify(paths)
 
-
+# method for handling uploadProjectFiles request
+# header:
+#   projectName: path where data will be stored.
+#                actually only final folder, everything
+#                else will be constructed in method
+# anatomical: 3D graymatter file (already segmented, normalized, etc)
+# functional: 4D fMRI file
+# return: JSON object with success=true if OK.
+#
+# method performs project folder init, by
+# storing original files, creating meta file with
+# project info (info.JSON)
 @app.route("/uploadProjectFiles",methods=['POST'])
 @cross_origin()
 def uploadProjectHandler():
@@ -58,6 +99,17 @@ def uploadProjectHandler():
         json.dump(data, file)
     return jsonify({'success':True}) 
 
+
+# method for handling uploadProjectFiles request
+# path: encoded string with desired data
+#
+# method return requested file for downloading or visualization.
+# NOTE: encoded string desription
+# http://SERVER/data/XXXX@YYYY[@ZZZZ]
+# XXXX - file (project name)
+# YYYY - "BIG" for anatomical quality
+#        "NORM" for functrional quality (based on fMRI resolution)
+# ZZZZ - percellations (if needed, otherwise will be original)
 @app.route('/data/<path:path>')
 @cross_origin()
 def send_volume(path):
@@ -88,6 +140,11 @@ def send_volume(path):
     else:
         return "not found"
 
+
+# method for handling /info/ request
+# path: encoded string with desired data (project name)
+#
+# send info.json of specific project to the client
 @app.route('/info/<path>')
 @cross_origin()
 def send_info(path):
@@ -97,6 +154,13 @@ def send_info(path):
         data = json.load(fp)
     return data
 
+
+# method for handling /parcellate request
+# header:
+#   projectName: name of the project
+#             K: desired amount of clusters
+#
+# perform parcellation for the file projectName, tries to reach K clusters
 @app.route('/parcellate')
 @cross_origin()
 def parcellate():
@@ -115,6 +179,14 @@ def parcellate():
     thread.start()
     return jsonify({'success':True})  
 
+
+# method for handling /calcCorr request
+# header:
+#   projectName: name of the project
+#    corrThresh: threshold for Pearson correlation value
+#    maskThresh: threshold for mask voxel signal
+#
+# Calculate the correlation coefficients matrix
 @app.route('/calcCorr')
 @cross_origin()
 def calcCorr():
@@ -136,6 +208,11 @@ def calcCorr():
     thread.start()
     return jsonify({'success':True}) 
 
+
+# method for handling /delete/ request
+# path : project name
+#
+# Delete whole project folder from the server
 @app.route('/delete/<path>')
 @cross_origin()
 def remove_project(path):
@@ -146,6 +223,9 @@ def remove_project(path):
     rmdir(fStr)
     return "done"
 
+
+# method for handling default request
+# stub that returns sample data.
 @app.route("/",methods=["GET"])
 def stub():
     return "What are you doing here :|?"
